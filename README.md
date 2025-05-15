9c7ffedd85536f01cb61d0a18b1566d2f8fd12cc
jodysimpson <jksimpson@ucdavis.edu>
2025-05-15 14:16:37 -0700

    Added scripts

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# bigfix
+# Bigfix
+
+Installs Bigfix client for UCD Linux systems
+
+Borrowed from Reuben Castelino from here: https://ucdavis.box.com/shared/static/r9tir3w6jd4oei83mbzhg6kg7ze4yp3y.sh
+
+## Usage
+
+`wget -q -O - https://github.com/UCD-DART/bigfix/blob/main/install-bigfix-debian.sh | sudo bash -s - install 072057`
---
+++ install-bigfix-debian.sh
@@ -0,0 +1,50 @@
+#! /bin/bash
+# Author: Reuben Castelino
+# Modified by: Jody Simpson <jksimpson@ucdavis.edu>
+# Purpose: To install bigfix on Linux systems at UC Davis
+# Usage 1: sudo ./install_bigfix.sh install ######
+# Use the coeadmintools to get the 6 digit department code for a user
+# Usage 2: sudo ./install_bigfix.sh uninstall
+
+step=$1
+dep_code=$2
+
+
+if [[ $step == "install" ]]
+then
+    if ! [[ "$2" =~ ^[0-9]+$ ]]
+    then
+        echo "Department code needs to be a 6 digit code: non-numerical input"
+        exit 1
+    fi
+    
+    if [[ ${#2} -ne 6 ]]
+    then
+        echo "Department code needs to be a 6 digit code: incorrect length"
+        exit 1
+    fi
+
+    # Install BigFix
+    wget https://software.bigfix.com/download/bes/110/BESAgent-11.0.4.60-debian10.amd64.deb
+    wget https://support.bigfix.com/bes/release/11.0/patch4/SHA256SUMS -O - | grep BESAgent-11.0.4.60-debian10.amd64.deb > SHA256
+    wget https://support.bigfix.com/bes/release/11.0/patch4/SHA1SUMS -O - | grep BESAgent-11.0.4.60-debian10.amd64.deb > SHA1
+    sha256sum -c SHA256 && sha1sum -c SHA1 && {
+    mkdir -p /etc/opt/BESClient/ || exit
+    wget --no-check-certificate "https://getbigfix.ucdavis.edu/generate_installer.php?reqtype=O&dept=$2" -O bigfix.zip
+    unzip bigfix.zip -d /etc/opt/BESClient/ || exit
+    dpkg -i BESAgent-11.0.4.60-debian10.amd64.deb
+    mv /etc/opt/BESClient/besclient.config /var/opt/BESClient
+    /etc/init.d/besclient start
+    } || echo "Install FAILED"
+
+elif [[ $step == "uninstall" ]]
+then
+    rm -rf /etc/opt/BESClient
+    rm -rf /var/opt/BESClient
+    dpkg --purge BESAgent
+
+else
+    echo "Command not understood, please use install or uninstall"
+fi
+
+
